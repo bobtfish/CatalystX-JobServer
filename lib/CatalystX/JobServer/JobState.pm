a72af0bb860702941a325bf0c13a42690e28e587
Tomas Doran <bobtfish@bobtfish.net>
2010-05-30 22:56:25 +0100

    Clean up code a bit

--- lib/CatalystX/JobServer/JobState.pm
+++ lib/CatalystX/JobServer/JobState.pm
@@ -6,6 +6,8 @@ use Moose::Autobox;
 use MooseX::Types::Set::Object;
 use MooseX::Storage::Engine;
 use aliased 'CatalystX::JobServer::Job::Running';
+use aliased 'CatalystX::JobServer::Job::Finished';
+
 use namespace::autoclean;
 
 MooseX::Storage::Engine->add_custom_type_handler(
@@ -40,6 +42,9 @@ has jobs_running => (
     traits => ['Serialize'],
 );
 
+before _add_running => sub { shift->_add_forked_worker(shift) };
+after _remove_running => sub { shift->_delete_forked_worker(shift) };
+
 has jobs_registered => (
     is => 'ro',
     isa => ArrayRef[Str],
@@ -49,27 +54,28 @@ has jobs_registered => (
 
 sub BUILD {
     my $self = shift;
-    foreach my $job (@{ $self->jobs_registered }) {
+    foreach my $job (@{ $self->jobs_registered }) { # Horrible
         Class::MOP::load_class($job);
     }
 }
 
 sub run_job {
-    my ($self, $job) = @_;
+    my ($self, $job, $return_cb) = @_;
     $job = Running->new(job => $job);
-    $self->_add_forked_worker;
     $self->_add_running($job);
+    # What happens about many many requets..
     fork_call {
         $job->job->run;
     }
     sub {
         $self->_remove_running($job);
-        $self->_delete_forked_worker;
         if (scalar @_) {
-            warn("Job ran, returned " . shift);
+            warn("Job ran, returned " . @_);
+            $return_cb->(Finished->new(job => $job));
         }
         else {
             warn("Job failed, returned " . $@);
+            $return_cb->(Finished->new(job => $job, ok => 0));
         }
     };
 }

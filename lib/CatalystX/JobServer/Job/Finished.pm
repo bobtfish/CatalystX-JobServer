4e4dd4b1d28c87e4f7ce629fba4d6732d0ffaffd
Tomas Doran <bobtfish@bobtfish.net>
2010-05-31 17:27:15 +0100

    Remember to add the class for jobs which have finished

---
+++ lib/CatalystX/JobServer/Job/Finished.pm
@@ -0,0 +1,40 @@
+package CatalystX::JobServer::Job::Finished;
+use Moose;
+use MooseX::Types::ISO8601 qw/ ISO8601DateTimeStr /;
+use MooseX::Types::Moose qw/ Bool /;
+use namespace::autoclean;
+
+extends 'CatalystX::JobServer::Job::Running';
+
+# FIXME - Gross, use a TC?
+around BUILDARGS => sub {
+    my ($orig, $self, @args) = @_;
+    my $args = $self->$orig(@args);
+    $args->{running_job} = delete $args->{job};
+    $args->{job} = $args->{running_job}->job;
+    $args->{start_time} = $args->{running_job}->start_time;
+    return $args;
+};
+
+has ok => (
+    isa => Bool,
+    is => 'ro',
+    default => 1,
+);
+
+has running_job => (
+    isa => 'CatalystX::JobServer::Job::Running',
+    is => 'ro',
+    required => 1,
+);
+
+has finish_time => (
+    isa => ISO8601DateTimeStr,
+    is => 'ro',
+    coerce => 1,
+    # FIXME - But with just time()
+    default => sub { DateTime->now },
+    traits => ['Serialize']
+);
+
+__PACKAGE__->meta->make_immutable;

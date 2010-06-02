65e6ad550a7547344541686e35daafd9eb41f7a1
Tomas Doran <bobtfish@bobtfish.net>
2010-06-03 00:46:29 +0100

    Add top level boiler plate. Remove unused VERSION from webapp, regen README

---
+++ lib/CatalystX/JobServer.pm
@@ -0,0 +1,26 @@
+package CatalystX::JobServer;
+use strict;
+use warnings;
+
+our $VERSION = '0.000001';
+
+1;
+
+=head1 NAME
+
+CatalystX::JobServer
+
+=head1 SEE ALSO
+
+L<CatalystX::JobServer::Web>, L<Net::RabbitFoot>.
+
+=head1 AUTHOR
+
+Tomas Doran
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

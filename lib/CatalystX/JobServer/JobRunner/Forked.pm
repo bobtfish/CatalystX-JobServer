43beb15845373d52bc4e9a5b72a4a745977fdb55
Tomas Doran <bobtfish@bobtfish.net>
2010-06-13 06:59:09 +0100

    No callback for you forked process

--- lib/CatalystX/JobServer/JobRunner/Forked.pm
+++ lib/CatalystX/JobServer/JobRunner/Forked.pm
@@ -12,9 +12,9 @@ sub post_fork {
 sub _do_run_job {
     my ($self, $job, $return_cb) = @_;
     # What happens about many many requets..
-    fork_call {
+    fork_call { # DO NOT ENTER THE EVENT LOOP OR YOU WILL DIE!!!
         $self->_clear_publish_timer if $self->can('_clear_publish_timer');
-        $self->post_fork($job, $return_cb);
+        $self->post_fork($job);
         $job->run;
     }
     sub {

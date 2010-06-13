58aca343cb2dfb7101b242a9dab95fa6ddcbdeea
Tomas Doran <bobtfish@bobtfish.net>
2010-06-13 02:26:24 +0100

    QueueListener now has sane error handling

--- lib/CatalystX/JobServer/Role/QueueListener.pm
+++ lib/CatalystX/JobServer/Role/QueueListener.pm
@@ -1,19 +1,50 @@
 package CatalystX::JobServer::Role::QueueListener;
 use CatalystX::JobServer::Moose::Role;
 use JSON qw/ decode_json /;
+use MooseX::Types::Moose qw/ Bool /;
+use Try::Tiny;
 
 requires 'act_on_message';
 
+has debug_messages_consumed => (
+    isa => Bool,
+    is => 'ro',
+    default => 0,
+);
+
 sub consume_message {
     my ($self, $message, $publisher) = @_;
-    print $message->{deliver}->method_frame->routing_key,
-        ': ', $message->{body}->payload, "\n";
-    # FIXME - deal with not being able to unserialize
-    my $data = decode_json($message->{body}->payload);
-    my $class = $data->{__CLASS__}; # FIXME - Deal with bad class.
+
+    my $payload = $message->{body}->payload;
+
+    warn blessed($self) .
+        ' got: ', $payload, "\n"
+        if $self->debug_messages_consumed;
+
+    my $data;
+    try {
+        $data = decode_json($payload);
+    }
+    catch {
+        warn(blessed($self) . "could not decode JSON, error: $_ message $payload\n");
+        return;
+    };
+
+    my $class = $data->{__CLASS__};
+
     unless ($class) {
+        warn(blessed($self) . " no __CLASS__ in message, cannot unpack: $payload\n");
         return;
     }
+    unless (Class::MOP::is_class_loaded($class)) {
+        warn(blessed($self) . " class $class not loaded, cannot unpack! ($payload)\n");
+        return;
+    }
+    unless ($class->can('unpack')) {
+        warn(blessed($self) . " class $class does not have an ->unpack method ($payload)\n");
+        return;
+    }
+
     my $object = $class->unpack($data);
     $self->act_on_message($object, $publisher);
 }

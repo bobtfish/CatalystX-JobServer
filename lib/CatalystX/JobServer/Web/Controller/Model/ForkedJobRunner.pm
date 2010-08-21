c2ce6c8d1ba1bf6137b46dbb27b8eacc96d47f1a
Tomas Doran <bobtfish@bobtfish.net>
2010-08-21 17:56:08 +0100

    And a form

--- lib/CatalystX/JobServer/Web/Controller/Model/ForkedJobRunner.pm
+++ lib/CatalystX/JobServer/Web/Controller/Model/ForkedJobRunner.pm
@@ -4,6 +4,7 @@ use MooseX::Types::Moose qw/ HashRef /;
 use MooseX::Types::LoadableClass qw/ LoadableClass /;
 use JSON::XS;
 use Form::Functional::Reflector::Metaclass;
+use Form::Functional::Renderer::TD;
 
 BEGIN { extends 'Catalyst::Controller' }
 
@@ -45,13 +46,21 @@ sub find_job : Chained('base') PathPart('') CaptureArgs(1) {
 
 sub display_job : Chained('find_job') PathPart('') Args(0) {
     my ($self, $c) = @_;
-    my $reflector = Form::Functional::Reflector::MetaClass->new(
+    my $rx_reflector = Form::Functional::Reflector::MetaClass->new(
         field_outputter_class => 'Form::Functional::Reflector::FieldOutputter::Rx',
         field_composer_class => 'Form::Functional::Reflector::FieldComposer::Rx',
     );
-    $c->res->body(JSON::XS->new->pretty(1)->encode(
-        $reflector->generate_output_from( $self->find_job_meta($c->stash->{job_name}) )
-    ));
+    my $form_reflector = Form::Functional::Reflector::MetaClass->new();
+    my $job_meta = $self->find_job_meta($c->stash->{job_name});
+
+    my $form = $form_reflector->generate_output_from($job_meta->name);
+    $c->res->body(
+        q{<html><body><h1>Rx</h1><pre>} .
+        JSON::XS->new->pretty(1)->encode($rx_reflector->generate_output_from( $job_meta ))
+        . q{</pre><h1>Form:</h1>}
+        . Form::Functional::Renderer::TD->new->render($form)
+        . q{</body></html>}
+    );
 }
 
 

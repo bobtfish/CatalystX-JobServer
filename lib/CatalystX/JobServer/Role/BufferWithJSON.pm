59433bf42167983d82acc0bbb514d6436bd489f7
Tomas Doran <bobtfish@bobtfish.net>
2010-10-31 09:54:16 +0000

    Extra paranoia

--- lib/CatalystX/JobServer/Role/BufferWithJSON.pm
+++ lib/CatalystX/JobServer/Role/BufferWithJSON.pm
@@ -9,7 +9,9 @@ method get_json_from_buffer ($buf_ref, $code_ref) {
         my $json = substr($$buf_ref, 0, $index+1, '');
         substr($json, length($json)-1, 1, ''); # Chop trailing \xff
         substr($json, 0, 1, '');               # Chop leading  \x00
-        $code_ref->(from_json($json));
+        my $data = try { from_json($json) }
+            catch { warn("Error deserializing $_ content $json") };
+        $code_ref->($data) if $data;
         return 1;
     }
     return;

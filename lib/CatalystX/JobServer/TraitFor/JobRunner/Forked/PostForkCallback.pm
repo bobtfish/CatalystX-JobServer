c3dca61d632c17659b2dda6eec082b2f7ad57284
Tomas Doran <bobtfish@bobtfish.net>
2010-12-01 19:45:59 +0000

    Delete unused stuff

--- lib/CatalystX/JobServer/TraitFor/JobRunner/Forked/PostForkCallback.pm
+++
@@ -1,8 +0,0 @@
-package CatalystX::JobServer::TraitFor::JobRunner::Forked::PostForkCallback;
-use CatalystX::JobServer::Moose::Role;
-
-#with 'CatalystX::JobServer::Role::CallbackWrapper' => {
-#    wrap => 'post_fork',
-#};
-
-1;
--- lib/CatalystX/JobServer/TraitFor/JobRunner/JobFailedCallback.pm
+++
@@ -1,8 +0,0 @@
-package CatalystX::JobServer::TraitFor::JobRunner::JobFailedCallback;
-use CatalystX::JobServer::Moose::Role;
-
-with 'CatalystX::JobServer::Role::CallbackWrapper' => {
-    wrap => 'job_failed',
-};
-
-1;

58782abc0b569a8bff2e1096d3a25fee2c01bdc2
Tomas Doran <bobtfish@bobtfish.net>
2010-12-03 19:26:06 +0000

    Tidy up JS, fix display bug.

--- root/static/jobdisplay.js
+++ root/static/jobdisplay.js
@@ -16,6 +16,7 @@ Module("CatalystX.JobServer.JobRunner.Forked.WorkerStatus", function (m) {
         },
         methods: {
             handleDisplay: function( display ) {
+                display.paintRowIfNeeded(this.uuid);
                 var percentage = Math.floor(100 * parseInt(this.steps_taken) / parseInt(this.step_count));
                 $("#progressBar" + this.uuid).progressBar(percentage, { boxImage: '/static/images/progressbar.gif', barImage: '/static/images/progressbg.gif'});
             }
@@ -34,6 +35,7 @@ Module("CatalystX.JobServer.JobRunner.Forked.WorkerStatus", function (m) {
         },
         methods: {
             handleDisplay: function( display ) {
+                display.paintRowIfNeeded(this.uuid);
                 $("#statusBar" + this.uuid).text(this.getStatus_info());
             }
         }
@@ -50,11 +52,9 @@ Module("CatalystX.JobServer.JobRunner.Forked.WorkerStatus", function (m) {
         },
         methods: {
             handleDisplay: function( display ) {
-                $(display.getSelector()).append('<tr id="' + this.job.uuid + '"><td class="uuid">'
-                    + this.job.uuid + '</td><td class="status" id="statusBar' + this.job.uuid + '"></td><td id="progressBar'
-                    + this.job.uuid + '"></td></tr>');
-                $("#statusBar" + this.job.uuid).text("Queued...");
-                display.getRunning_jobs()[this.job.uuid] = 1;
+                display.paintRowIfNeeded(this.uuid);
+                $("#statusBar" + this.uuid).text("Queued...");
+                display.getRunning_jobs()[this.uuid] = 1;
             }
         }
     });
@@ -69,6 +69,7 @@ Module("CatalystX.JobServer.Job", function (m) {
         },
         methods: {
             handleDisplay: function( display ) {
+                display.paintRowIfNeeded(this.job.uuid);
                 $("#statusBar" + this.job.uuid).text("Started new job");
             }
         }
@@ -80,11 +81,12 @@ Module("CatalystX.JobServer.Job", function (m) {
                 is: "ro",
             },
             progress_bar_selector: {
-               is: "rw",
+                is: "rw",
             }
         },
         methods: {
             handleDisplay: function( display ) {
+                display.paintRowIfNeeded(this.job.uuid);
                 $("#progressBar" + this.job.uuid).progressBar(100);
                 delete display.getRunning_jobs()[this.job.uuid];
             }
@@ -93,57 +95,62 @@ Module("CatalystX.JobServer.Job", function (m) {
 });
 
 Module("CatalystX.JobServer.JobDisplay", function (m) {
-   Class("Hippie", {
-       has: {
-           hippie: {
-               is: "ro",
-               init: function () {
-                   var that = this;
-                   new Hippie(
-                       document.location.host,
-                       5, // Timeout
-                       function() { that.connected() },
-                       function() { that.disconnected() },
-                       function(e) { that.handleMessage(e) },
-                       that.hippie_path                                                  
-                   );
-               },
-               lazy: 1
-           },
-           running_jobs: {
-               is: "ro",
-               init: {},
-           },
-           selector: {
-               is: "ro"
-           }
-       },
-       methods: {
-           handleMessage: function (m) {
-               var message;
-               try {
-                     message = Joose.Storage.Unpacker.unpack(m);
-                     if (!this.getRunning_jobs()[message.uuid]) {
-                         $(this.getSelector()).append('<tr id="' + message.uuid + '"><td class="uuid">' + message.uuid + '</td><td class="status" id="statusBar' + message.uuid + '"></td><td id="progressBar' + message.uuid + '"></td></tr>');
-                         $("#progressBar" + message.uuid).progressBar({ boxImage: '/static/images/progressbar.gif', barImage: '/static/images/progressbg.gif'});
-                         this.getRunning_jobs()[message.uuid] = 1;
-                    }
-                     
-                    message.handleDisplay(this); // I love a nice bit of double dispatch
-                 } catch (ex) {
-                     log_it("got unhandled message, exception: " + ex + " message : " + dump(m));
-                 }
-           },
-           connected: function () {},
-           disconnected: function () {},
-           displayInside: function (selector, hippie_path) {
-               this.selector = selector;
-               this.hippie_path = hippie_path;
-               this.getHippie();
-               $(this.selector)
-           }
-       }
-   });
+    Class("Hippie", {
+        has: {
+            hippie: {
+                is: "ro",
+                init: function () {
+                    var that = this;
+                    new Hippie(
+                        document.location.host,
+                        5, // Timeout
+                        function() { that.connected() },
+                        function() { that.disconnected() },
+                        function(e) { that.handleMessage(e) },
+                        that.hippie_path
+                    );
+                },
+                lazy: 1
+            },
+            running_jobs: {
+                is: "ro",
+                init: {},
+            },
+            selector: {
+                is: "ro"
+            }
+        },
+        methods: {
+            handleMessage: function (m) {
+                var message;
+                try {
+                    message = Joose.Storage.Unpacker.unpack(m);
+                    message.handleDisplay(this) ; // I love a nice bit of double dispatch
+                } catch (ex) {
+                    log_it("got unhandled message, exception: '" + ex + "' message : " + dump(m));
+                };
+            },
+            paintRowIfNeeded: function (uuid) {
+                if (!this.getRunning_jobs()[uuid]) {
+                    $(this.getSelector()).append(
+                        '<tr id="' + uuid + '"><td class="uuid">'
+                        + uuid + '</td><td class="status" id="statusBar'
+                        + uuid + '"></td><td id="progressBar' + uuid + '"></td></tr>'
+                    );
+                    $("#progressBar" + uuid).progressBar({ boxImage: '/static/images/progressbar.gif', barImage: '/static/images/progressbg.gif'});
+                    this.getRunning_jobs()[uuid] = 1;
+                }
+            },
+            connected: function () {},
+            disconnected: function () {},
+            displayInside: function (selector, hippie_path) {
+                this.selector = selector;
+                this.hippie_path = hippie_path;
+                this.getHippie();
+                $(this.selector)
+            }
+        }
+    });
 });
      
 function log_it(stuff) {

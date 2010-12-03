610ed6983a52277d3cbcf950dda9eb9028e3e43b
Tomas Doran <bobtfish@bobtfish.net>
2010-12-03 00:45:28 +0000

    Switch to Twiggy.
    It did have the same shitty issue as Corona, but I just patched it to
    fix that, and I'd like to get Coro out of my stack as it should be unneeded

---
+++ lib/CatalystX/JobServer/Web/Script/Server.pm
@@ -0,0 +1,81 @@
+package CatalystX::JobServer::Web::Script::Server;
+use CatalystX::JobServer::Moose;
+use CatalystX::JobServer ();
+use Twiggy::Server;
+use Plack::App::URLMap;
+use Plack::App::File;
+use Plack::App::Cascade;
+use Try::Tiny;
+use MooseX::Types::Common::Numeric qw/PositiveInt/;
+use MooseX::Types::Moose qw/ Str Bool /;
+use Catalyst::Utils ();
+use namespace::autoclean;
+
+with 'Catalyst::ScriptRole';
+
+has debug => (
+    traits        => [qw(Getopt)],
+    cmd_aliases   => 'd',
+    isa           => Bool,
+    is            => 'ro',
+    documentation => q{Force debug mode},
+);
+
+has host => (
+    traits        => [qw(Getopt)],
+    cmd_aliases   => 'h',
+    isa           => Str,
+    is            => 'ro',
+    # N.B. undef (the default) means we bind on all interfaces on the host.
+    documentation => 'Specify a hostname or IP on this host for the server to bind to',
+);
+
+has port => (
+    traits        => [qw(Getopt)],
+    cmd_aliases   => 'p',
+    isa           => PositiveInt,
+    is            => 'ro',
+    default       => sub {
+        Catalyst::Utils::env_value(shift->application_name, 'port') || 5000
+    },
+    documentation => 'Specify a different listening port (to the default port 5000)',
+);
+
+sub _run_application {
+    my $self = shift;
+    my $app = $self->application_name;
+
+    local $ENV{CATALYST_DEBUG} = 1
+            if $self->debug;
+
+    Class::MOP::load_class($app);
+
+    # Can $::TERMINATE->throw to exit :)
+    $::TERMINATE = AnyEvent->condvar;
+    $::RUNNING = AnyEvent->condvar;
+
+    my $map = Plack::App::URLMap->new;
+
+    $map->map('/static' => Plack::App::Cascade->new(
+        apps => [
+            eval { require Web::Hippie::App::JSFiles; 1; } ? Web::Hippie::App::JSFiles->new->to_app : (),
+            Plack::App::File->new(
+                root => $app->path_to(qw/root static/)
+            )->to_app,
+        ]
+    ));
+
+    $map->map('/' => sub { CatalystX::JobServer::Web->run(@_) });
+
+    my $server = Twiggy::Server->new(
+        host => $self->host,
+        port => $self->port,
+    );
+    $server->register_service($map->to_app);
+
+    $::RUNNING->send;
+    $::TERMINATE->recv;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
--- script/catalystx_jobserver.psgi
+++ script/catalystx_jobserver.psgi
@@ -1,53 +1,39 @@
 #!/usr/bin/env perl
-use strict;
-use warnings;
-use FindBin qw/$Bin/;
-use lib "$Bin/../lib";
-use CatalystX::JobServer ();
-use Twiggy;
-use AnyEvent;
-use aliased 'CatalystX::JobServer::Web';
-use Plack::Runner;
-use Plack::App::URLMap;
-use Plack::App::File;
-use Plack::App::Cascade;
-use Try::Tiny;
 
-use DateTime;
-
-{   # Give all warnings timestamps, useful for logging / debugging.
-    my $oldwarn = $SIG{__WARN__} || \&CORE::warn;
-    $SIG{__WARN__} = sub { my $msg = DateTime->now . " " . shift(); unshift(@_, $msg); goto $oldwarn };
-}
-
-# Can $::TERMINATE->throw to exit :)
-our $TERMINATE = AnyEvent->condvar;
-our $RUNNING = AnyEvent->condvar;
-
-my $map = Plack::App::URLMap->new;
-
-$map->map('/static' => Plack::App::Cascade->new(
-    apps => [
-        eval { require Web::Hippie::App::JSFiles } ? Web::Hippie::App::JSFiles->new->to_app : (),
-        Plack::App::File->new(
-            root => CatalystX::JobServer::Web->path_to(qw/root static/)
-        )->to_app,
-    ]
-));
-
-$map->map('/' => sub { CatalystX::JobServer::Web->run(@_) });
-
-my $host = '127.0.0.1';
-my $port = '5000';
-
-use Twiggy::Server;
-my $server = Twiggy::Server->new(
-     host => $host,
-      port => $port,
-);
-$server->register_service($map->to_app);
-
-$RUNNING->send;
-$TERMINATE->recv;
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('CatalystX::JobServer::Web', 'Server');
 
 1;
+
+=head1 NAME
+
+catalystx_jobserver_.psgi - Catalyst Test Server
+
+=head1 SYNOPSIS
+
+catalystx_jobserver_.psgi [options]
+
+   -d --debug           force debug mode
+   -? --help            display this help and exits
+   -h --host            host (defaults to all)
+   -p --port            port (defaults to 3000)
+
+ See also:
+   perldoc Catalyst::Manual
+   perldoc Catalyst::Manual::Intro
+
+=head1 DESCRIPTION
+
+Run this application.
+
+=head1 AUTHORS
+
+Tomas Doran
+
+=head1 COPYRIGHT
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
